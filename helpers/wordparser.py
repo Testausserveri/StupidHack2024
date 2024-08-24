276ff5d6ed57ac2f44ba23d8316f4e154c65944b
Hege <heikki@testausserveri.fi>
2024-08-24 14:08:15 +0300

    Remove unused wordparser code

--- helpers/wordparser.py
+++ helpers/wordparser.py
@@ -13,35 +13,6 @@ def recurse_word(phrase, answer, tree):
         tree[word] = answer
 
 
-def generate(prompt):
-    req = requests.post('http://localhost:11434/api/generate',
-                 json={"model":"dolphin-mixtral","prompt":prompt})
-
-    ans = ""
-    for line in req.iter_lines():
-        print(line)
-        line_obj = json.loads(line)
-        if line_obj['done'] == "true":
-            break
-        ans += str(line_obj['response'])
-
-    print(ans)
-    return ans
-
-
-def main():
-    with open("phrases.txt", "r") as f:
-        prompts = [x.strip() for x in f.readlines()]
-
-    print(len(prompts))
-    with open("ai_out.csv", "w") as w:
-        for prompt in prompts:
-            print(f"generating: {prompt}")
-            answer = generate(prompt)
-            csv_writer = csv.writer(w)
-            csv_writer.writerow([prompt, answer])
-
-
 def write_if(file, current_indentation, tree):
     for word, answer in tree.items():
         file.write(" "*4*current_indentation + f"if input[{current_indentation-1}] == {repr(word)}:\n")
